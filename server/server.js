a4bfc455fefbfde74444fa3c7849b5539607378d
Corey Garvin <cgarvin@apartments247.com>
2019-08-23 17:12:59 -0700

    organized

--- server/server.js
+++ server/server.js
@@ -4,31 +4,35 @@ const app = express();
 const port = 3000;
 const soundsRoot = "/Users/corey/Downloads/music/";
 const soundPlayer = require("play-sound")(opts = {});
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get("/", (req, res) => res.send("Dude, there's nothing here!"));
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get("/", (req, res) => res.send("Dude, there's nothing here!"));
 
 // Get a tree of files/folders
 app.get("/listing", (req, res) => {
     const filteredTree = dirTree(soundsRoot, { extensions: /\.(wav|mp3|)$/i });
     trimPath(filteredTree);
     res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(filteredTree));
-})
+    res.end(JSON.stringify(filteredTree.children));
+});
 
+// Play a sound
 app.post("/play", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     soundPlayer.play(soundsRoot + req.body.file, function(err) {
         console.log("Could not find/play audio file: " + soundsRoot + req.body.file);
     });
     res.end(JSON.stringify({"status": "thank you come again"}));
-})
+});
 
 function trimPath(tree) {
-    // Adjust the paths to be relative to our sounds dir
+    // Adjust the paths to be relative to our sounds dir, instead of showing
+    // the absolute path
     if (!tree) {
         return;
     }
@@ -38,4 +42,4 @@ function trimPath(tree) {
 
 
 // Start server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Soundboard Server listening on port ${port}!`));
