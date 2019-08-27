2bdfb36967c9868ea9b0cf807526de74bf1cb65f
JustinTheeDude <JustinTheeDude@gmail.com>
2019-08-27 16:58:30 -0700

    ajkdsjfka'

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,6 +1,26 @@
+Vue.component('sound', {
+    props: ['image'],
+    template:  '<img class="play-button" :id="image.idTag" :src="image.img" />'
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello'
+        imgList: [
+            {id: 0, idTag: "peon", img: "img/peon.png"},
+            {id: 1, idTag: "peasent", img: "img/peasant.png"}
+        ],
+        info: []
+    },
+
+    mounted() {
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => {
+                console.log(response);
+                for(var item in response) {
+                    this.info.push(item);
+                }
+                console.log(this.info);
+            });
     }
 })
